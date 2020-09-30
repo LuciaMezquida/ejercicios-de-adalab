9b965d6fe7f8d81f92b8a2d36a3a3ddf4fee7b8c
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-09-30 19:03:13 +0200

    Put html content inside a div with js

---
+++ modulo-2/Leccion-1/ejercicio6-lorem-ipsum/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let container = document.querySelector(".container");
+
+container.innerHTML =
+  "<h1>Lorem Ipsum</h1><img src='http://via.placeholder.com/350x150'><p>Cien cañones por banda, viento en popa a toda vela</p>";
