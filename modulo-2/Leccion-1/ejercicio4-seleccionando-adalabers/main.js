e34328a9134f813def4bfc0f249b5f3947152197
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-09-30 18:37:56 +0200

    Select by querySelector and modify with innerHTML

---
+++ modulo-2/Leccion-1/ejercicio4-seleccionando-adalabers/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let title = document.querySelector(".title");
+console.log(title);
+
+let adalaber = document.querySelector(".partner1");
+console.log(adalaber);
+
+title.innerHTML = title.innerHTML + " " + adalaber.innerHTML;
