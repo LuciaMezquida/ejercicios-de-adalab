bef352e85735ed519f933d49418f4f4d985673d6
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-04 17:44:42 +0200

    use if statement

---
+++ modulo-2/leccion-3/ejercicio2-control-de-acceso/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const name = prompt("Introduce un nombre");
+
+if (name === "Emma") {
+  alert("Bienvenida Emma");
+} else {
+  alert("Lo siento pero el usuario que has introducido no está registrado");
+}
