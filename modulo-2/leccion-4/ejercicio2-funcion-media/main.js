a8c1455ce23a07c15ff649aa474f529805d4271a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-05 17:24:31 +0200

    add media function

---
+++ modulo-2/leccion-4/ejercicio2-funcion-media/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function media(a, b, c, d) {
+  return (a + b + c + d) / 4;
+}
+
+console.log(media(4, 7, 99, 4)); //28.5
+console.log(media(3, 5, 2, 4)); //3.5
+console.log(media(12, 7, 4, 22)); //11.25
