be96ed6e6d7c5e49f0e747f0ae505b3026acc667
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-05 17:34:38 +0200

    add iva function

--- modulo-2/leccion-4/ejercicio3-ticket-con-iva/main.js
+++ modulo-2/leccion-4/ejercicio3-ticket-con-iva/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const priceWithIva = function (price) {
+  const iva = price * 0.21;
+  const price_iva = price + iva;
+  return `Precio sin iva: ${price}, IVA: ${iva}, y Total: ${price_iva}`;
+};
+
+console.log(priceWithIva(20));
