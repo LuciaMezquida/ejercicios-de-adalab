575a461f7b3b7cc974c1b0c2e1edef943fd306f8
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-07 17:06:10 +0200

    investigate the event object

---
+++ modulo-2/leccion-6/modulo-2-leccion-6-ejercicio-4/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const button = document.querySelector(".button");
+button.addEventListener("click", function (event) {
+  console.log(event);
+});
+
+//la clave type es una propiedad--> type: 'click'
