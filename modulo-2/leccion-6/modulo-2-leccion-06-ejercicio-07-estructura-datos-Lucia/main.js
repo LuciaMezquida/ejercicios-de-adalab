22a2b5fbb6aeb83a56932ed87180bbe11648a480
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-07 18:29:56 +0200

    add user data structure

---
+++ modulo-2/leccion-6/modulo-2-leccion-06-ejercicio-07-estructura-datos-Lucia/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const job = "developer";
+const object = {};
+
+object.firstName = "Lucia";
+object.lastName = "Mezquida";
+object.age = 38;
+object.job = job;
+
+console.log(object.firstName);
+console.log(object.lastName);
+console.log(object.age);
+console.log(object.job);
+
+object.firstName = "Maria";
+object.age = 39;
+
+console.log(object.firstName);
+console.log(object.age);
