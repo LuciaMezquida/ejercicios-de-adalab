c2d4bfc8b111135b54f730fd5cbebeee428511af
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-07 08:52:27 +0200

    fix bug

--- modulo-2/leccion-5/ejercicio10-que-vemos-esta-noche/main.js
+++ modulo-2/leccion-5/ejercicio10-que-vemos-esta-noche/main.js
@@ -9,26 +9,26 @@ const button = document.querySelector(".button");
 button.addEventListener("click", function () {
   const movie = document.querySelector(".movie");
   movie.innerHTML = `<ul>
-  <li class="inception">${inception}</li>
-  <li class="butterfly">${theButterFlyEffect}</li>
-  <li class="sunshine">${eternalSunshineOfTheSM}</li>
-  <li class="velvet">${blueVelvet}</li>
-  <li class="split">${split}</li>
-</ul>`;
+    <li class="inception">${inception}</li>
+    <li class="butterfly">${theButterFlyEffect}</li>
+    <li class="sunshine">${eternalSunshineOfTheSM}</li>
+    <li class="velvet">${blueVelvet}</li>
+    <li class="split">${split}</li>
+  </ul>`;
+
+  const inceptionMovie = document.querySelector(".inception");
+  const butterflyMovie = document.querySelector(".butterfly");
+  const sunshineMovie = document.querySelector(".sunshine");
+  const velvetMovie = document.querySelector(".velvet");
+  const splitMovie = document.querySelector(".split");
+
+  function printMovie(event) {
+    console.log(event.currentTarget.innerHTML);
+  }
+
+  inceptionMovie.addEventListener("click", printMovie);
+  butterflyMovie.addEventListener("click", printMovie);
+  sunshineMovie.addEventListener("click", printMovie);
+  velvetMovie.addEventListener("click", printMovie);
+  splitMovie.addEventListener("click", printMovie);
 });
-
-const inceptionMovie = document.querySelector(".inception");
-const butterflyMovie = document.querySelector(".butterfly");
-const sunshineMovie = document.querySelector(".sunshine");
-const velvetMovie = document.querySelector(".velvet");
-const splitMovie = document.querySelector(".split");
-
-function printMovie(event) {
-  console.log(event.currentTarget.innerHTML);
-}
-
-inceptionMovie.addEventListener("click", printMovie);
-butterflyMovie.addEventListener("click", printMovie);
-sunshineMovie.addEventListener("click", printMovie);
-velvetMovie.addEventListener("click", printMovie);
-splitMovie.addEventListener("click", printMovie);
