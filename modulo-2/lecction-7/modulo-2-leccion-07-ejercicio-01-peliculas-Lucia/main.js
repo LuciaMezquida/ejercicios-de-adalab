9765ffe9771e70822d447f3e8692ac9e90490c9c
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-08 18:06:06 +0200

    add loop for

---
+++ modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-01-peliculas-Lucia/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const movies = ["Amelie", "Lo que el viento se llevó", "Kill Bill"];
+
+function workWithMovies() {}
---
+++ modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-02-bucle-Lucia/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+for (let i = 1; i <= 10; i++) {
+  console.log("Voy por la vuelta " + i);
+}
