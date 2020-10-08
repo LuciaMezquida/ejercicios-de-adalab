578ca4cef67916586fed14c2c8a0e87cf114bcff
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-08 18:57:46 +0200

    add more examples

--- modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-05-la-media-Lucia/main.js
+++ modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-05-la-media-Lucia/main.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const numbers = [2, 8, 11, 4, 9, 5];
+const moreNumbers = [22, 56, 8, 12, 9, 31];
 let acc = 0;
 
 function average(arr) {
   for (let i = 0; i < arr.length; i++) {
-    acc += numbers[i];
+    acc += arr[i];
   }
   return "La media es igual a: " + acc / arr.length;
 }
 
 console.log(average(numbers));
+console.log(average(moreNumbers));
