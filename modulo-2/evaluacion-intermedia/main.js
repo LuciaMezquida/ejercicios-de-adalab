96afc26f87803259857bd97a51408da859c2f816
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-09 12:29:09 +0200

    create event

--- modulo-2/evaluacion-intermedia/main.js
+++ modulo-2/evaluacion-intermedia/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const face = document.querySelector('.js-face');
+const btn = document.querySelector('.js-button');
+
+function updateClick (){
+  console.log('hola')
+}
+
+btn.addEventListener('click', updateClick)
