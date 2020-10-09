7d9840b7c4830415d3db22730edff76dcf1c4570
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-09 12:14:39 +0200

    exercise with pair

--- modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-01-peliculas-Lucia/main.js
+++ modulo-2/lecction-7/modulo-2-leccion-07-ejercicio-01-peliculas-Lucia/main.js
@@ -1,5 +1,11 @@
 "use strict";
 
 const movies = ["Amelie", "Lo que el viento se llevó", "Kill Bill"];
+function workWithMovies() {
+  movies[3] = "Parásitos";
+  movies[2] = "Interestelar";
+  movies[1] = "El Rey León";
+  console.log(movies)
+}
 
-function workWithMovies() {}
+workWithMovies()
