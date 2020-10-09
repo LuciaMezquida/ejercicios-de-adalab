6b6e596dae09c1ae5f7cdd53bb69de720031c163
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-09 13:17:40 +0200

    change background color with a random number

--- modulo-2/evaluacion-intermedia/main.js
+++ modulo-2/evaluacion-intermedia/main.js
@@ -2,9 +2,18 @@
 
 const face = document.querySelector('.js-face');
 const btn = document.querySelector('.js-button');
+const container = document.querySelector('.js-container');
+
 
 function updateClick (){
-  console.log('hola')
+  const select = document.querySelector('.js-select').value;
+  face.innerHTML = select;
+  let numRandom = Math.floor(Math.random() * 100)
+  if (numRandom % 2 !== 0) {
+    container.classList.add('orange')
+  } else {
+    container.classList.remove('orange')
+  }
 }
 
 btn.addEventListener('click', updateClick)
