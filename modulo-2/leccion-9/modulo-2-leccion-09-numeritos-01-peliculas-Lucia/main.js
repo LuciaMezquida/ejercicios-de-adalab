6641c8ab900150f4f98edf909ba41815b25ba0ff
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-13 18:52:55 +0200

    Use push method

---
+++ modulo-2/leccion-9/modulo-2-leccion-09-numeritos-01-peliculas-Lucia/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function get100Numbers () {
+  let arrayHundred = []
+  for (let i = 1; i <= 100; i++) {
+    arrayHundred.push(i);
+    console.log(arrayHundred);
+  }
+}
+get100Numbers()
