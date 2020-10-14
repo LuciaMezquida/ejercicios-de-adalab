527b2e5ba9dd7015fbb6220c15ce5d65e4e156bf
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-14 22:36:30 +0200

    Finish christmas tree!!!

--- modulo-2/leccion-9/modulo-2-leccion-09-ejercicio-07-abeto-Lucia/main.js
+++ modulo-2/leccion-9/modulo-2-leccion-09-ejercicio-07-abeto-Lucia/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 function christmasTree() {
-  let acc = ''
-  console.log('★')
-  for (let i = 1; i <= 5; i++) {
-    acc += '▲'
-    console.log(acc) 
+  let triangle = '▲'
+  let spaces = ' '
+  console.log('    ★')
+  for (let numTriangles = 1, numSpaces = 4; numTriangles <= 9; numTriangles+=2, numSpaces--) {
+    let moreSpaces = spaces.repeat(numSpaces)
+    let moreTriangles = triangle.repeat(numTriangles)
+    console.log(moreSpaces + moreTriangles) 
   }
-  console.log('|')
+  console.log('    |')
 }
 christmasTree()
 
