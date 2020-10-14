00864ed92b90e165c5003deac59357572db52d68
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-14 19:56:34 +0200

    First attemp christmas tree

---
+++ modulo-2/leccion-9/modulo-2-leccion-09-ejercicio-07-abeto-Lucia/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function christmasTree() {
+  let acc = ''
+  console.log('★')
+  for (let i = 1; i <= 5; i++) {
+    acc += '▲'
+    console.log(acc) 
+  }
+  console.log('|')
+}
+christmasTree()
+
