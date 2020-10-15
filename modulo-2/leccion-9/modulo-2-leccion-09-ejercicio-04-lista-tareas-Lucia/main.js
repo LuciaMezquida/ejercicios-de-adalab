5c89303004eb7e42cc9a2b2979cc49061d5fd151
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-15 10:35:57 +0200

    Complete exercise 4

--- modulo-2/leccion-9/modulo-2-leccion-09-ejercicio-04-lista-tareas-Lucia/main.js
+++ modulo-2/leccion-9/modulo-2-leccion-09-ejercicio-04-lista-tareas-Lucia/main.js
@@ -9,26 +9,57 @@ const tasks = [
     completed: false
   }
 ];
+//Mostrar cuántas tareas hay
+const container = document.querySelector('.container');
+container.innerHTML += `<p>Hay ${tasks.length} tareas</p>`;
 
-function createList(item) {
-  const list = document.querySelector('.js-list')
-  return list.innerHTML = `<li>${item}</li>`
-}
+//Añado párrafo para las Tareas totales
+container.innerHTML += `<p class="results"></p>`;
 
+//Pintar todas las tareas en la pantalla, tachar las ya realizadas
+container.innerHTML += `<ul class="list"></ul>`;
+const list = document.querySelector('.list');
 
-function showNumberTasks () {
-  const task = document.querySelector('.js-tasks')
-  return task.innerHTML = `Hay ${tasks.length} tareas`
-  
-}
-showNumberTasks()
-
-function showTasks () {
-  const task1 = document.querySelector('.js-tasks1')
-  for (let i = 0; i < tasks.length; i++) {
-    createList(tasks[i].name)
+for (let i = 0; i < tasks.length; i++) {
+  list.innerHTML += `<li>${tasks[i].name}</li>`;
+  let lastItem = list.lastChild
+  if (tasks[i].completed === true) {
+    lastItem.classList.add('check'); //tachar las ya realizadas
+    const complete = `<input type="checkbox" checked class="checked"/>`; //añadir checkbox 
+    lastItem.innerHTML += complete;
+  } else {
+    const uncomplete = `<input type="checkbox" class="unchecked"/>`;//añadir checkbox
+    lastItem.innerHTML += uncomplete;
   }
 }
-showTasks()
 
+//Marcar una tarea como completa o incompleta
+const checkbox = document.querySelectorAll('input');//selecciono todos los inputs que he creado
+for (let i = 0; i < checkbox.length; i++) {
+  checkbox[i].addEventListener('click', changeCheck) //al hacer click me desencadena la funcion
+}
+function changeCheck() {
+  const listItems = document.querySelectorAll('li');
+  for (let i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked === true) { 
+      checkbox[i].classList.add('checked')
+      checkbox[i].classList.remove('unchecked')
+      tasks[i].completed = true;
+      listItems[i].classList.add('check')
+    } else {
+      checkbox[i].classList.add('unchecked')
+      checkbox[i].classList.remove('checked')
+      tasks[i].completed = false;
+      listItems[i].classList.remove('check')
+    }
+  }
+  
+  //Calculo las tareas totales que irán en el segundo párrafo
+  const results = document.querySelector('.results');
+  const tasksCompleted = document.querySelectorAll('.checked')
+  const tasksUncompleted = document.querySelectorAll('.unchecked')
+  results.innerHTML = `Tienes ${tasks.length} tareas. ${tasksCompleted.length} completadas y ${tasksUncompleted.length} por realizar`
+}
 
+//Llamo a la función para que se pinte el párrafo de las tareas totales al iniciar la página
+changeCheck()
