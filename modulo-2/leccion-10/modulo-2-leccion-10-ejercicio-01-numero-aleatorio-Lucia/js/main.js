d58da65aa0e44904ae6c5ab92a5d44e8f2dffe50
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-15 18:49:18 +0200

    Use API direction

--- modulo-2/leccion-10/modulo-2-leccion-10-ejercicio-01-numero-aleatorio-Lucia/js/main.js
+++ modulo-2/leccion-10/modulo-2-leccion-10-ejercicio-01-numero-aleatorio-Lucia/js/main.js
@@ -1,9 +1,9 @@
 'use strict'
-function getEmoji() {
-  fetch("https://api.rand.fun/games/rockpaperscissorslizardspock")
+function getNumber() {
+  fetch("https://api.rand.fun/number/integer?length=4")
     .then(response => response.json())
     .then(data => {
       document.querySelector('.js-result').innerHTML = data.result
     });
 }
-document.querySelector('.js-emoji').addEventListener("click", getEmoji);
+document.querySelector('.js-number').addEventListener("click", getNumber);
