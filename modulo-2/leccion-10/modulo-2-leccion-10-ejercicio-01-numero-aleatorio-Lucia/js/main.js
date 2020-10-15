ffe379dce40466c4dd322146b8d15c671877e9ef
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-15 18:32:00 +0200

    Change file distribution

---
+++ modulo-2/leccion-10/modulo-2-leccion-10-ejercicio-01-numero-aleatorio-Lucia/js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+function getEmoji() {
+  fetch("https://api.rand.fun/games/rockpaperscissorslizardspock")
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('.js-result').innerHTML = data.result
+    });
+}
+document.querySelector('.js-emoji').addEventListener("click", getEmoji);
