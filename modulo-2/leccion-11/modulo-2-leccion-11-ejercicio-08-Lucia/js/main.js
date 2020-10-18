3442636400c431e6ee36c14fc4647f506b79daf8
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-18 21:40:05 +0200

    Catch server searches with a comparator function before .then

---
+++ modulo-2/leccion-11/modulo-2-leccion-11-ejercicio-08-Lucia/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let result = document.querySelector(".js-result");
+let characterInfo = {
+  name: "Nombre",
+  gender: "Género",
+};
+
+function getCharacter() {
+  let character = document.querySelector(".js-cast").value;
+  if (localStorage.getItem(character)) {
+    recoveryInfo(character);
+  } else {
+    fetch(`https://swapi.dev/api/people/?search=${character}`)
+      .then((response) => response.json())
+      .then((data) => {
+        result.innerHTML += `<li>Personaje: ${data.results[0].name}. Género: ${data.results[0].gender}</li>`;
+        characterInfo.name = data.results[0].name;
+        characterInfo.gender = data.results[0].gender;
+        localStorage.setItem(character, JSON.stringify(characterInfo));
+      });
+  }
+}
+document.querySelector(".js-btn").addEventListener("click", getCharacter);
+
+function recoveryInfo(characterInput) {
+  const info = JSON.parse(localStorage.getItem(characterInput));
+  console.log(info);
+  result.innerHTML += `<li>Personaje: ${info.name}. Género: ${info.gender}</li>`;
+}
