f08dc49e40052a9775d73872980d11e0df665b6f
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-19 13:26:55 +0200

    Create html with createElement, createTextNode and appendChild

--- modulo-2/leccion-12/modulo-2-leccion-12-ejercicio-01-1-2-3-Lucia/js/main.js
+++ modulo-2/leccion-12/modulo-2-leccion-12-ejercicio-01-1-2-3-Lucia/js/main.js
@@ -1,3 +1,10 @@
 "use strict";
 
 const numbers = [1, 2, 3];
+const listContainer = document.querySelector(".js-list");
+for (let i = 0; i < numbers.length; i++) {
+  const list = document.createElement("li");
+  const listContent = document.createTextNode(numbers[i]);
+  list.appendChild(listContent);
+  listContainer.appendChild(list);
+}
