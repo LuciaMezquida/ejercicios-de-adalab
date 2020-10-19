6759c27cbc81240a4327d2745e62f5b7c97a956e
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-19 10:24:42 +0200

    Add array with users info

---
+++ modulo-2/leccion-12/modulo-2-leccion-12-ejercicio-03-1-autocompletad-Lucia/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const usersInfo = [
+  {
+    nombre: "Yamira",
+    apellido: "Martin",
+    telefono: 555666777,
+  },
+  {
+    nombre: "Nuria",
+    apellido: "Díaz",
+    telefono: 111222333,
+  },
+  {
+    nombre: "Lucia",
+    apellido: "Mezquida",
+    telefono: 888999000,
+  },
+];
