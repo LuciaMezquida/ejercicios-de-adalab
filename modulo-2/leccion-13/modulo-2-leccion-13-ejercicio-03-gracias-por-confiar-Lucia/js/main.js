705df450d2825dafe7b3a2420d61fa06013fd0b6
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 10:02:24 +0200

    Use map to add messages

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-03-gracias-por-confiar-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-03-gracias-por-confiar-Lucia/js/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const users = [
+  { name: "María", isPremium: false },
+  { name: "Lucía", isPremium: true },
+  { name: "Susana", isPremium: true },
+  { name: "Rocío", isPremium: false },
+  { name: "Inmaculada", isPremium: false },
+];
+let usersPremium = [];
+let noPremium = [];
+for (let i = 0; i < users.length; i++) {
+  if (users[i].isPremium === true) {
+    usersPremium = users.map((item) => `Bienvenida ${item}. Gracias por confiar en nosotros.`);
+  } else {
+    noPremium = users.map((item) => `Bienvenida ${item}`);
+  }
+}
+console.log(usersPremium);
+
+console.log(noPremium);
