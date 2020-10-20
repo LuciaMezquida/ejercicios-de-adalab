d1248864a3fead841da1ccc52b7dcff30d580662
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 10:07:39 +0200

    Use filter to get premium users

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-04-solo-los-premium-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-04-solo-los-premium-Lucia/js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const users = [
+  { name: "María", isPremium: false },
+  { name: "Lucía", isPremium: true },
+  { name: "Susana", isPremium: true },
+  { name: "Rocío", isPremium: false },
+  { name: "Inmaculada", isPremium: false },
+];
+
+const premiumUsers = users.filter((item) => item.isPremium);
+console.log(premiumUsers);
