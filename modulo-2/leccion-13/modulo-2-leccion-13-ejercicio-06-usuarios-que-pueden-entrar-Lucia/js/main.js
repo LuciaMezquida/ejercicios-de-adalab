e048b7bd469bb0d24cf527c966c9590de4e91c5c
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 10:14:23 +0200

    Use filter and map to get the names of even pin users

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-06-usuarios-que-pueden-entrar-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-06-usuarios-que-pueden-entrar-Lucia/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const users = [
+  { name: "María", isPremium: false, pin: 2389 },
+  { name: "Lucía", isPremium: true, pin: 2384 },
+  { name: "Susana", isPremium: true, pin: 2837 },
+  { name: "Rocío", isPremium: false, pin: 5232 },
+  { name: "Inmaculada", isPremium: false, pin: 8998 },
+];
+
+const usersEvenPin = users.filter((item) => item.pin % 2 === 0);
+const usersEvenPinNames = usersEvenPin.map((item) => item.name);
+console.log(usersEvenPinNames);
