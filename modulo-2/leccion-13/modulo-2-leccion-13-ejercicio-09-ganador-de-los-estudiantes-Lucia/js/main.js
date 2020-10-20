1d1923e0f8b9ee8e4b2924d6f8f7b3965fa1c537
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 14:12:32 +0200

    Use filter and reduce to get de winning student

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-09-ganador-de-los-estudiantes-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-09-ganador-de-los-estudiantes-Lucia/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const runners = [
+  { name: "Gregory Goyle", time: 56, student: true },
+  { name: "Nymphadora Tonks", time: 9, student: false },
+  { name: "Luna Lovegood", time: 45, student: true },
+  { name: "Cedric Diggory", time: 28, student: true },
+  { name: "Cho Chang", time: 35, student: true },
+];
+
+const studentWinner = runners
+  .filter((item) => item.student)
+  .reduce((acc, item) => {
+    return acc.time < item.time ? acc : item;
+  });
+console.log("The winner is: " + studentWinner.name);
