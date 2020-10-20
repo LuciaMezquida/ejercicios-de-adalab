0e6cf31d5887431c50692604a40c21f41e90f2f9
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 12:25:15 +0200

    Use reduce to get the winner

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-08-ganador-carrera-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-08-ganador-carrera-Lucia/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+const runners = [
+  { name: "Gregory Goyle", time: 56 },
+  { name: "Nymphadora Tonks", time: 9 },
+  { name: "Luna Lovegood", time: 45 },
+  { name: "Cedric Diggory", time: 28 },
+  { name: "Cho Chang", time: 35 },
+];
+
+const winner = runners.reduce((acc, item) => {
+  return acc.time < item.time ? acc : item;
+});
+console.log(winner);
