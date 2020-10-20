b0b6e2ab423d6726aa41f7cb82092609cad76034
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 14:30:09 +0200

    Use find and findIndex to get an specific item

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-10-encuentra-el-usuario-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-10-encuentra-el-usuario-Lucia/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+const users = [
+  { name: "María", isPremium: false, pin: 2389 },
+  { name: "Lucía", isPremium: true, pin: 2384 },
+  { name: "Susana", isPremium: true, pin: 2837 },
+  { name: "Rocío", isPremium: false, pin: 5232 },
+  { name: "Inmaculada", isPremium: false, pin: 8998 },
+];
+const incidenceUser = users.find((user) => user.pin === 5232);
+console.log("Es usuario de la incidencia es: " + incidenceUser.name);
+
+const userToErase = users.findIndex((user) => user.pin === 5232);
+users.splice(userToErase, 1);
+console.log(users);
