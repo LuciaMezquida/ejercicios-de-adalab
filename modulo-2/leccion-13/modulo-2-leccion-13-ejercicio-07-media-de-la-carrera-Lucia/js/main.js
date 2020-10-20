c47cf41ce9f86b2ef78bbb080b090ff323f2ef47
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 10:27:42 +0200

    Use reduc to get the mean of the numbers

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-07-media-de-la-carrera-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-07-media-de-la-carrera-Lucia/js/main.js
@@ -1,1 +1,6 @@
 "use strict";
+
+const times = [56, 9, 45, 28, 35];
+const sumTimes = times.reduce((acc, item) => acc + item);
+const meanNumbers = sumTimes / times.length;
+console.log(meanNumbers);
