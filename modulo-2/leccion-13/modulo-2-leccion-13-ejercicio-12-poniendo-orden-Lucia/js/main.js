64c0402446638d86f3d90e1a2d25315e6b8e555b
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 14:50:22 +0200

    Sort names and numbers with sort()

--- modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-12-poniendo-orden-Lucia/js/main.js
+++ modulo-2/leccion-13/modulo-2-leccion-13-ejercicio-12-poniendo-orden-Lucia/js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const users = [
+  { name: "María", isPremium: false, pin: 2389 },
+  { name: "Lucía", isPremium: true, pin: 2384 },
+  { name: "Susana", isPremium: true, pin: 2837 },
+  { name: "Rocío", isPremium: false, pin: 5232 },
+  { name: "Inmaculada", isPremium: false, pin: 8998 },
+];
+//ordenar por nombre
+//sacado de: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/sort
+users.sort(function (a, b) {
+  if (a.name > b.name) {
+    return 1;
+  }
+  if (a.name < b.name) {
+    return -1;
+  }
+  return 0;
+});
+console.log(users);
+
+//Ordenar por pin
+users.sort((a, b) => a.pin - b.pin);
+console.log(users);
