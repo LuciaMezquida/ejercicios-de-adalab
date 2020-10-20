81c663f76a37fd6a96c666fbe4c9ed0bb7bf8af5
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-20 09:06:25 +0200

    Add color to each paragraph with its own select

--- modulo-2/leccion-12/modulo-2-leccion-12-ejercicio-06-castigo-Lucia/js/main.js
+++ modulo-2/leccion-12/modulo-2-leccion-12-ejercicio-06-castigo-Lucia/js/main.js
@@ -5,9 +5,10 @@ for (let i = 0; i < 100; i++) {
   let paragraph = document.createElement("p"); //creo un párrafo
   let text = document.createTextNode("He aprendido bien cómo funcionan los bucles"); //creo texto
   let select = document.createElement("select"); //creo un select
+  select.setAttribute("id", i); //añado id a cada elemento select
   for (let j = 0; j < colors.length; j++) {
     let option = document.createElement("option"); //creo un option para cada color
-    option.setAttribute("value", colors[j]);
+    option.setAttribute("value", colors[j]); //añado value a cada option
     let color = document.createTextNode(colors[j]); //creo un texto para cada option
     option.appendChild(color); //añado los textos a option
     select.appendChild(option); //añado los option al select
@@ -17,25 +18,29 @@ for (let i = 0; i < 100; i++) {
   container.appendChild(select); //añado los select al body, despues de cada párrafo
 }
 
-const allSelects = document.querySelectorAll("select");
-const allParagraphs = document.querySelectorAll("p");
+const allSelects = document.querySelectorAll("select"); //array de elementos select
+const allParagraphs = document.querySelectorAll("p"); //array de párrafos
 function changeColor(event) {
-  console.log(event.target.selectedIndex);
-  if (event.target.value === "white") {
-    allParagraphs[0].style.color = "white";
-  } else if (event.target.value === "blue") {
-    allParagraphs[0].style.color = "blue";
-  } else if (event.target.value === "red") {
-    allParagraphs[0].style.color = "red";
-  } else if (event.target.value === "green") {
-    allParagraphs[0].style.color = "green";
-  } else if (event.target.value === "yellow") {
-    allParagraphs[0].style.color = "yellow";
-  } else if (event.target.value === "pink") {
-    allParagraphs[0].style.color = "pink";
+  for (let i = 0; i < allParagraphs.length; i++) {
+    if (parseInt(event.currentTarget.id) === i) {
+      if (event.target.value === "white") {
+        allParagraphs[i].style.color = "white";
+      } else if (event.target.value === "blue") {
+        allParagraphs[i].style.color = "blue";
+      } else if (event.target.value === "red") {
+        allParagraphs[i].style.color = "red";
+      } else if (event.target.value === "green") {
+        allParagraphs[i].style.color = "green";
+      } else if (event.target.value === "yellow") {
+        allParagraphs[i].style.color = "yellow";
+      } else if (event.target.value === "pink") {
+        allParagraphs[i].style.color = "pink";
+      }
+    }
   }
 }
 
 for (let item of allSelects) {
+  //añado un evento a cada select
   item.addEventListener("change", changeColor);
 }
