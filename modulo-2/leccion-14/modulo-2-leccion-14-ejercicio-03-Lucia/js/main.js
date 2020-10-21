8b620ba3a48af6ffdd39d56f645d64765a794aa1
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 15:28:50 +0200

    Change message after 1 minute

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-03-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-03-Lucia/js/main.js
@@ -1,1 +1,16 @@
 "use strict";
+let counter = 0;
+const time = document.querySelector(".js-message");
+
+const incrementAndShowCounter = () => {
+  counter++;
+
+  if (counter === 1) {
+    time.innerHTML = `Escrito hace ${counter} segundo`;
+  }
+  if (counter > 1 && counter < 60) {
+    time.innerHTML = `Escrito hace ${counter} segundos`;
+  } else if (counter >= 60) time.innerHTML = `Escrito hace 1 min`;
+};
+
+setInterval(incrementAndShowCounter, 1000);
