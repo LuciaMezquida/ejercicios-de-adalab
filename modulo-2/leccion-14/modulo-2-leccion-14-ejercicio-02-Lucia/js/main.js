c65ba2dd72101e6bc52c951cb952e0f13425754b
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 10:19:17 +0200

    Stop counter with conditions

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-02-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-02-Lucia/js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+let counter = 0;
+
+const incrementAndShowCounter = () => {
+  if (counter < 12) {
+    counter++;
+    const time = document.querySelector(".js-counter");
+    time.innerHTML = counter;
+  }
+};
+
+setInterval(incrementAndShowCounter, 1000);
