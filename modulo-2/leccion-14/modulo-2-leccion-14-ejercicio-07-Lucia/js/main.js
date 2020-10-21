872fbe26302d49e8b9534c96594449c11636439c
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 17:15:45 +0200

    Add button to restart every 10 second

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-07-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-07-Lucia/js/main.js
@@ -1,1 +1,15 @@
 "use strict";
+const btn = document.querySelector(".js-btn");
+const removeMsg = () => {
+  btn.innerHTML = "¿Te has dormido?";
+};
+
+let timer = setTimeout(removeMsg, 10000);
+
+let counter = 0;
+const restartCounter = () => {
+  btn.innerHTML = "";
+  timer = setTimeout(removeMsg, 10000);
+};
+
+btn.addEventListener("click", restartCounter);
