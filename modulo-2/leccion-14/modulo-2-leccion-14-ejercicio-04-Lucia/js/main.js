d30a12f13dacb14108edf8b847b13a3dd063f1dd
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 15:44:40 +0200

    Use setTimeout to get a window after 15 seconds

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-04-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-04-Lucia/js/main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const removeMsg = () => {
+  const window = document.querySelector(".window");
+  window.innerHTML = "Su sesión ha expirado";
+  window.classList.add("windowRed");
+};
+
+setTimeout(removeMsg, 15000);
