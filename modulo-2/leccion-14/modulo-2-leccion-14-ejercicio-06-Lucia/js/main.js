02433e47e7c0ac922c4fdf6d0d73d3aa333cd412
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 16:39:45 +0200

    Add a chronometer

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-06-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-06-Lucia/js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const counterPlace = document.querySelector(".counter");
+const stop = document.querySelector(".stop");
+const cont = document.querySelector(".continue");
+
+let counter = 0;
+let temp;
+counterPlace.innerHTML = "0";
+
+const incrementAndShowCounter = () => {
+  counter++;
+  counterPlace.innerHTML = counter;
+};
+const stopCounter = () => {
+  clearInterval(temp);
+};
+const contCounter = () => {
+  temp = setInterval(incrementAndShowCounter, 1000);
+  incrementAndShowCounter();
+};
+
+temp = setInterval(incrementAndShowCounter, 1000);
+stop.addEventListener("click", stopCounter);
+cont.addEventListener("click", contCounter);
