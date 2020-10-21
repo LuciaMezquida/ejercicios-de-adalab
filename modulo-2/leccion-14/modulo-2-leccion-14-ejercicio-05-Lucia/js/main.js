8fb937715ebae4e3e053953b810a63f782f71020
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-21 15:52:05 +0200

    Stop setInterval with clearInterval

--- modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-05-Lucia/js/main.js
+++ modulo-2/leccion-14/modulo-2-leccion-14-ejercicio-05-Lucia/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+let counter = 0;
+
+const incrementAndShowCounter = () => {
+  counter++;
+  const time = document.querySelector(".js-counter");
+  time.innerHTML = counter;
+  if (counter === 12) {
+    clearInterval(temp);
+  }
+};
+
+let temp = setInterval(incrementAndShowCounter, 1000);
