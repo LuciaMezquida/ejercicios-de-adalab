8a788d5ef1406ba37f07c90b38ee449f95c8fa24
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-29 20:54:41 +0100

    Add 'hola mundo' in app.js

--- modulo-3/leccion-02/modulo-3-leccion-02-ejercicio-01-Lucia/my-react-project/src/App.js
+++ modulo-3/leccion-02/modulo-3-leccion-02-ejercicio-01-Lucia/my-react-project/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hola mundo, no sé descansar. Soy una yonki de la programación</p>
         <a
           className="App-link"
           href="https://reactjs.org"
