104d9d19c0b851f62664594af4dd648284cf944e
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-29 18:01:07 +0100

    Add attributes to the class

--- modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-02-Lucia/js/main.js
+++ modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-02-Lucia/js/main.js
@@ -1,1 +1,25 @@
 "use strict";
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+  perimeter() {
+    return this.side * 4;
+  }
+  area() {
+    return Math.pow(this.side, 2);
+  }
+}
+
+const oneSquare = new Square(1);
+const threeSquare = new Square(3);
+const sevenSquare = new Square(7);
+
+console.log(oneSquare.perimeter());
+console.log(oneSquare.area());
+
+console.log(threeSquare.perimeter());
+console.log(threeSquare.area());
+
+console.log(sevenSquare.perimeter());
+console.log(sevenSquare.area());
