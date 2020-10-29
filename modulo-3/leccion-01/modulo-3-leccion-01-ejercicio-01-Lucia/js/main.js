9bd774fa5259bc13c0eaf459fa2f802f5b2099b6
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-29 17:24:41 +0100

    Add methods to square class

--- modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-01-Lucia/js/main.js
+++ modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-01-Lucia/js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+class Square {
+  perimeter(side) {
+    return side * 4;
+  }
+  area(side) {
+    return Math.pow(side, 2);
+  }
+}
+
+const oneSquare = new Square();
+console.log(oneSquare.perimeter(9));
+console.log(oneSquare.area(9));
