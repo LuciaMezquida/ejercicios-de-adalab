74ea716d8488d39ddaecb863aeb29dc1ddeff7d4
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-10-29 18:49:20 +0100

    Add heritage to class polygon

--- modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-03-Lucia/js/main.js
+++ modulo-3/leccion-01/modulo-3-leccion-01-ejercicio-03-Lucia/js/main.js
@@ -1,1 +1,37 @@
 "use strict";
+class Polygon {
+  constructor(numberOfSides, base, height) {
+    this.numberOfSides = numberOfSides;
+    this.base = base;
+    this.height = height;
+  }
+  perimeter() {
+    return this.base * this.numberOfSides;
+  }
+  area() {
+    return this.base * this.height;
+  }
+}
+
+class Square extends Polygon {
+  constructor(side) {
+    super(4, side, side);
+    this.side = side;
+  }
+}
+class Triangle extends Polygon {
+  constructor(base, height) {
+    super(3, base, height);
+  }
+  area() {
+    return super.area() / 2;
+  }
+}
+
+const oneSquare = new Square(4);
+const oneTriangle = new Triangle(4, 3);
+
+console.log(oneSquare.area());
+console.log(oneSquare.perimeter());
+console.log(oneTriangle.area());
+console.log(oneTriangle.perimeter());
