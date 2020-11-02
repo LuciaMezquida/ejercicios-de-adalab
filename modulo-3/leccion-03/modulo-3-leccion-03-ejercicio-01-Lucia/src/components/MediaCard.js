0c3662ff8f830e74cbb442faa2d7aa5925367a52
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-02 18:29:37 +0100

    Transform class to function

--- modulo-3/leccion-03/modulo-3-leccion-03-ejercicio-01-Lucia/src/components/MediaCard.js
+++ modulo-3/leccion-03/modulo-3-leccion-03-ejercicio-01-Lucia/src/components/MediaCard.js
@@ -1,36 +1,30 @@
 import React from "react";
 import "../App.css";
 
-class MediaCard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <header className="App-header">
-            <img src={this.props.image} className="App-logo" alt="logo" />
-            <div className="title-container">
-              <p className="title">{this.props.name}</p>
-              <p>{this.props.date}</p>
-            </div>
-          </header>
-          <main>
-            <p className="main-text">{this.props.text}</p>
-          </main>
-          <footer className="footer">
-            <p className="footer-text">Leer más...</p>
-            <div className="likes-container">
-              <p>{this.props.likes}</p>
-              <i class="fas fa-heart">{this.props.heart}</i>
-            </div>
-          </footer>
-        </div>
+const MediaCard = (props) => {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <img src={props.image} className="App-logo" alt="logo" />
+          <div className="title-container">
+            <p className="title">{props.name}</p>
+            <p>{props.date}</p>
+          </div>
+        </header>
+        <main>
+          <p className="main-text">{props.text}</p>
+        </main>
+        <footer className="footer">
+          <p className="footer-text">Leer más...</p>
+          <div className="likes-container">
+            <p>{props.likes}</p>
+            <i class="fas fa-heart">{props.heart}</i>
+          </div>
+        </footer>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default MediaCard;
