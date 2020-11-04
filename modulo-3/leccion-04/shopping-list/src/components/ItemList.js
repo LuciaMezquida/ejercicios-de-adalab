15d467a2e82dbde152454dca1f6681024e24d1d1
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-04 18:14:04 +0100

    Add component to itemList

--- modulo-3/leccion-04/shopping-list/src/components/ItemList.js
+++ modulo-3/leccion-04/shopping-list/src/components/ItemList.js
@@ -27,51 +27,24 @@ const arrayOfItems = [
 
 class ItemList extends React.Component {
   render() {
-    // const listItems = arrayOfItems.map((item) => (
-    //   <li>
-    //     name={item.name}
-    //     description={item.description}
-    //     quantity={item.quantity}
-    //     category={item.category}
-    //     price={item.price}
-    //   </li>
-    const array = arrayOfItems.filter((item) => item.price < 10);
-    const name = array.map((item) => item.name);
-    const description = array.map((item) => item.description);
-    const quantity = array.map((item) => item.quantity);
-    const category = array.map((item) => item.category);
-    const price = array.map((item) => item.price);
-    return (
-      <ul className="item-list">
-        <li>
-          <Item
-            name={name[0]}
-            description={description[0]}
-            quantity={quantity[0]}
-            category={category[0]}
-            price={price[0]}
-          />
-        </li>
-        <li>
-          <Item
-            name={name[1]}
-            description={description[1]}
-            quantity={quantity[1]}
-            category={category[1]}
-            price={price[1]}
-          />
-        </li>
-        <li>
-          <Item
-            name={name[2]}
-            description={description[2]}
-            quantity={quantity[2]}
-            category={category[2]}
-            price={price[2]}
-          />
-        </li>
-      </ul>
-    );
+    const listItems = arrayOfItems.map((item) => (
+      <li>
+        <Item
+          name={item.name}
+          description={item.description}
+          quantity={item.quantity}
+          category={item.category}
+          price={item.price}
+        />
+      </li>
+    ));
+    // const array = arrayOfItems.filter((item) => item.price < 10);
+    // const name = array.map((item) => item.name);
+    // const description = array.map((item) => item.description);
+    // const quantity = array.map((item) => item.quantity);
+    // const category = array.map((item) => item.category);
+    // const price = array.map((item) => item.price);
+    return <ul className="item-list">{listItems}</ul>;
   }
 }
 export default ItemList;
