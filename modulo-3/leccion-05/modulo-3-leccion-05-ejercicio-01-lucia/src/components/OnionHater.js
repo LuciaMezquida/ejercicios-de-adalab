9c95f8224d42e73f7a93670390e8e182af29f313
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-05 18:39:35 +0100

    add background color red when 'cebolla' is written on textarea

--- modulo-3/leccion-05/modulo-3-leccion-05-ejercicio-01-lucia/src/components/OnionHater.js
+++ modulo-3/leccion-05/modulo-3-leccion-05-ejercicio-01-lucia/src/components/OnionHater.js
@@ -1,13 +1,29 @@
 import React from "react";
 
 class OnionHater extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isHating = false;
+    this.handleChange = this.handleChange.bind(this);
+  }
   handleChange(event) {
-    if (event.currentTarget.value.includes("cebolla")) {
-      alert("Odio la cebolla");
+    const container = document.querySelector(".container");
+    if (event.currentTarget.value.toLowerCase().includes("cebolla")) {
+      alert("ODIO LA CEBOLLA");
+      container.classList.add("red");
+      this.isHating = true;
+    } else {
+      container.classList.remove("red");
+      this.isHating = false;
     }
+    this.forceUpdate();
   }
   render() {
-    return <textarea className="App" onChange={this.handleChange}></textarea>;
+    return (
+      <div className="container">
+        <textarea className="App" onChange={this.handleChange}></textarea>;
+      </div>
+    );
   }
 }
 
