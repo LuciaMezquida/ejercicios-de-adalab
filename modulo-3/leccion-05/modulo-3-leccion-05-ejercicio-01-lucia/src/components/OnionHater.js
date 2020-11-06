ae6aabe64b7d780e23ad2c1a08ea5b868e236ed8
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-06 10:50:21 +0100

    Delete querySelector

--- modulo-3/leccion-05/modulo-3-leccion-05-ejercicio-01-lucia/src/components/OnionHater.js
+++ modulo-3/leccion-05/modulo-3-leccion-05-ejercicio-01-lucia/src/components/OnionHater.js
@@ -3,24 +3,21 @@ import React from "react";
 class OnionHater extends React.Component {
   constructor(props) {
     super(props);
-    this.isHating = false;
+    this.isHating = "";
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
-    const container = document.querySelector(".container");
     if (event.currentTarget.value.toLowerCase().includes("cebolla")) {
       alert("ODIO LA CEBOLLA");
-      container.classList.add("red");
-      this.isHating = true;
+      this.isHating = "red";
     } else {
-      container.classList.remove("red");
-      this.isHating = false;
+      this.isHating = "";
     }
     this.forceUpdate();
   }
   render() {
     return (
-      <div className="container">
+      <div className={"container " + this.isHating}>
         <textarea className="App" onChange={this.handleChange}></textarea>;
       </div>
     );
