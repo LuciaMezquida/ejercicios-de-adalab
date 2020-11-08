1071ee4ad000fc32a6fcd80370aa084c730ae332
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-08 20:17:50 +0100

    Add if statement to setState to change div color with every click

--- modulo-3/leccion-06/modulo-3-leccion-06-ejercicio-02-lucia/src/components/App.js
+++ modulo-3/leccion-06/modulo-3-leccion-06-ejercicio-02-lucia/src/components/App.js
@@ -7,10 +7,25 @@ class App extends React.Component {
     this.state = {
       background: "red",
     };
+    this.handleChangeColor = this.handleChangeColor.bind(this);
+  }
+  handleChangeColor() {
+    console.log("click");
+    this.setState((prevState, props) => {
+      let nextBackground;
+      if (prevState.background === "red") {
+        nextBackground = "green";
+      } else {
+        nextBackground = "red";
+      }
+      return {
+        background: nextBackground,
+      };
+    });
   }
 
   render() {
-    return <div className={"App " + this.state.background}></div>;
+    return <div className={"App " + this.state.background} onClick={this.handleChangeColor}></div>;
   }
 }
 
