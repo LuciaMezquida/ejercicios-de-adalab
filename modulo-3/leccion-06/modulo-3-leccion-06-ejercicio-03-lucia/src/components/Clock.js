87b08b3abb796bfc646d15846d80720f4218bc05
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-08 21:47:26 +0100

    Set actual  hour

---
+++ modulo-3/leccion-06/modulo-3-leccion-06-ejercicio-03-lucia/src/components/Clock.js
@@ -0,0 +1,32 @@
+import "../styleSheets/App.css";
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: 0,
+      minutos: 0,
+      segundos: 0,
+    };
+    this.updateClock = this.updateClock.bind(this);
+    setInterval(this.updateClock, 1000);
+  }
+  updateClock() {
+    let date = new Date();
+    this.setState({
+      hora: date.getHours(),
+      minutos: date.getMinutes(),
+      segundos: date.getSeconds(),
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <p className="clock">{`${this.state.hora} : ${this.state.minutos} : ${this.state.segundos}`}</p>
+      </div>
+    );
+  }
+}
+
+export default Clock;
