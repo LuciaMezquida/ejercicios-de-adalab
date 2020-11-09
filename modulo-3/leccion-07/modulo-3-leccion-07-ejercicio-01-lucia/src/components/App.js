f2994ffb1beb0c4c80fe6e6e76bb74a9ea67ca4a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-09 19:04:22 +0100

    Add 2 useState

--- modulo-3/leccion-07/modulo-3-leccion-07-ejercicio-01-lucia/src/components/App.js
+++ modulo-3/leccion-07/modulo-3-leccion-07-ejercicio-01-lucia/src/components/App.js
@@ -3,14 +3,23 @@ import React, { useState } from "react";
 
 function App() {
   const [number, setNumber] = useState(38);
-  const getOlder = () => {
+  const [gift, setGift] = useState(1);
+  // const getOlder = () => {
+  //   setNumber(number + 1);
+  // };
+  const getMoreGifts = () => {
     setNumber(number + 1);
+    setGift(gift + 1);
   };
   return (
     <div className="App">
       <p className="text">Hoy tengo {number} años de edad</p>
-      <button onClick={getOlder} className="btn">
+      {/* <button onClick={getOlder} className="btn">
         Hazme más viejo
+      </button> */}
+      <p className="text">Tengo {gift} regalos</p>
+      <button onClick={getMoreGifts} className="btn">
+        Dame más regalos
       </button>
     </div>
   );
