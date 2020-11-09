35fccf23b31341f845fb90a8f96dc82e816ef956
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-09 09:33:43 +0100

    Add counter with state

--- modulo-3/leccion-06/modulo-3-leccion-06-ejercicio-04-lucia/src/components/SheepCounter.js
+++ modulo-3/leccion-06/modulo-3-leccion-06-ejercicio-04-lucia/src/components/SheepCounter.js
@@ -3,14 +3,28 @@ import React from "react";
 class SheepCounter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      counter: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState((prevState, props) => {
+      prevState = prevState.counter + 1;
+      return {
+        counter: prevState,
+      };
+    });
   }
 
   render() {
+    console.log(this.state.counter);
     return (
       <>
-        <p className="text"></p>
-        <button className="btn">Más ovejas</button>
+        <p className="text">{this.state.counter}</p>
+        <button className="btn" onClick={this.handleClick} value="sheep">
+          Más ovejas
+        </button>
       </>
     );
   }
