3b7d90699a60c38f489af145533d06b36aba0b8a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-12 21:25:35 +0100

    Add functional component to make a form

--- modulo-3/leccion-08/modulo-3-leccion-08-ejercicio-01-lucia/src/components/App.js
+++ modulo-3/leccion-08/modulo-3-leccion-08-ejercicio-01-lucia/src/components/App.js
@@ -1,15 +1,60 @@
-import "../styleSheets/App.css";
-import React from "react";
-import Form from "./Form";
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+import "./App.css";
+import React, { useState, useRef } from "react";
+// import React, { useRef } from 'react';
+function App() {
+  const fr = new FileReader();
+  let textInput = useRef(null);
+  let introImage = useRef(null);
+  const [input, setInput] = useState("");
+  const [textarea, setTextarea] = useState("");
+  const [selectArea, setSelectArea] = useState("");
+  function handleInput(ev) {
+    if (ev.target.id === "input") {
+      setInput(ev.target.value);
+    } else if (ev.target.id === "textarea") {
+      setTextarea(ev.target.value);
+    } else if (ev.target.id === "select") {
+      setSelectArea(ev.target.value);
+    }
+    textInput.current.focus();
   }
-
-  render() {
-    return <Form />;
+  function getImage(e) {
+    const myFile = e.currentTarget.files[0];
+    fr.readAsDataURL(myFile);
   }
+  return (
+    <div className="App">
+      <form>
+        <label> Nombre </label>
+        <input ref={textInput} id="input" value={input} onChange={handleInput} />
+        <label>Descripción</label>
+        <textarea id="textarea" value={textarea} onChange={handleInput}></textarea>
+        <select id="select" value={selectArea} onChange={handleInput}>
+          <option value="español">Español</option>
+          <option value="inglés">Inglés</option>
+          <option value="portugués">Portugués</option>
+        </select>
+        <br />
+        <div class="action">
+          <button class="action__upload-btn js__profile-trigger" type="button">
+            Añadir imagen
+          </button>
+          <input
+            onChange={getImage}
+            type="file"
+            name=""
+            id="img-selector"
+            class="action__hiddenField js__profile-upload-btn"
+          />
+          <div class="profile">
+            <div class="visualize profile__preview js__profile-preview"></div>
+          </div>
+        </div>
+      </form>
+      <p>{"El nombre es: " + input}</p>
+      <p>{"La descripción es: " + textarea}</p>
+      <p>{"El idioma seleccionado es: " + selectArea}</p>
+    </div>
+  );
 }
-
 export default App;
