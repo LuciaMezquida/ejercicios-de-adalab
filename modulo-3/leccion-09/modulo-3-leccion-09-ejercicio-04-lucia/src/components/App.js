f1225a1f41738fd5555ee957dc9d3b27639a7ded
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-15 20:02:28 +0100

    Use destructuring arrays

--- modulo-3/leccion-09/modulo-3-leccion-09-ejercicio-04-lucia/src/components/App.js
+++ modulo-3/leccion-09/modulo-3-leccion-09-ejercicio-04-lucia/src/components/App.js
@@ -1,13 +1,30 @@
 import "../styleSheets/App.css";
 import React from "react";
 
+const users = [
+  { name: "Nymphadora Tonks", time: 9 },
+  { name: "Cedric Diggory", time: 28 },
+  { name: "Cho Chang", time: 35 },
+  { name: "Luna Lovegood", time: 45 },
+  { name: "Gregory Goyle", time: 56 },
+];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div className="App">Hola</div>;
+    const [name1, name2, name3] = users;
+    return (
+      <div className="App">
+        <ul>
+          <li key={name1.time}>{name1.name}</li>
+          <li key={name2.time}>{name2.name}</li>
+          <li key={name3.time}>{name3.name}</li>
+        </ul>
+      </div>
+    );
   }
 }
 
