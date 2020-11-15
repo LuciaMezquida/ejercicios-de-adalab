fd12be82479f5078f2250d7c731065bc84b6b75a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-15 18:16:25 +0100

    Create react app

---
+++ modulo-3/leccion-09/modulo-3-leccion-09-ejercicio-04-lucia/src/components/App.js
@@ -0,0 +1,14 @@
+import "../styleSheets/App.css";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="App">Hola</div>;
+  }
+}
+
+export default App;
