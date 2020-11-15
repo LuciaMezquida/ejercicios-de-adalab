3c1d5f6ae26467fdaf8af19388d2420cf57781fb
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-15 18:10:00 +0100

    Filter by even number with checkbox

--- modulo-3/leccion-09/modulo-3-leccion-09-ejercicio-01-lucia/src/components/App.js
+++ modulo-3/leccion-09/modulo-3-leccion-09-ejercicio-01-lucia/src/components/App.js
@@ -1,13 +1,56 @@
 import "../styleSheets/App.css";
 import React from "react";
 
+const numbers = [1, 4, 6, 8, 45, 89];
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listNumbers: "",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+  handleInput(ev) {
+    const itemInput = ev.currentTarget.value;
+    const listNumbers = numbers
+      .filter((item) => item > itemInput)
+      .map((item) => <li key={item}>{item}</li>);
+    this.setState({
+      listNumbers: listNumbers,
+    });
+  }
+  handleCheck(ev) {
+    if (ev.currentTarget.checked === true) {
+      const listFiltered = this.state.listNumbers;
+      const listNumbers = listFiltered.filter((item) => item.key % 2 === 0);
+
+      this.setState({
+        listNumbers: listNumbers,
+      });
+    }
   }
 
   render() {
-    return <div className="App">Hola</div>;
+    return (
+      <div className="App">
+        <form action="">
+          <input onChange={this.handleInput} type="text" />
+          <label htmlFor="even">
+            <input
+              onClick={this.handleCheck}
+              id="even"
+              type="checkbox"
+              value="chooseeven"
+              name="even"
+            />
+            Seleccionar sólo los pares
+          </label>
+        </form>
+        <ul>{this.state.listNumbers}</ul>
+      </div>
+    );
   }
 }
 
