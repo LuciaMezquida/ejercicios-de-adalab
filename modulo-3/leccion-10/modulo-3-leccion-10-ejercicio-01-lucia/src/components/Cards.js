68c3a432f597bad7f14c1bbb928f0e836b813084
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-16 15:48:11 +0100

    Create react app

---
+++ modulo-3/leccion-10/modulo-3-leccion-10-ejercicio-01-lucia/src/components/Cards.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+class Cards extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div className="image-container"></div>
+      </>
+    );
+  }
+}
+Cards.propTypes = {};
+export default Cards;
