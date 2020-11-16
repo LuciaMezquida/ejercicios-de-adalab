96fbd68735e7c8691a4320ed690d412a0b454f34
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-16 15:50:38 +0100

    Add hashRouter to index.js

--- modulo-3/leccion-11/modulo-3-leccion-11-ejercicio-01-lucia/src/index.js
+++ modulo-3/leccion-11/modulo-3-leccion-11-ejercicio-01-lucia/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import "./styleSheets/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById("root")
 );
 
