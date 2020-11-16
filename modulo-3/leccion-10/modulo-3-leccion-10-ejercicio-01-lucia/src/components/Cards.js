e6e95b2c5dcde88813e2a2fa2ed47df4aa93fe8e
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-16 17:29:28 +0100

    Add props to Card

--- modulo-3/leccion-10/modulo-3-leccion-10-ejercicio-01-lucia/src/components/Cards.js
+++ modulo-3/leccion-10/modulo-3-leccion-10-ejercicio-01-lucia/src/components/Cards.js
@@ -9,12 +9,12 @@ class Cards extends React.Component {
     return (
       <>
         <div className="image-container">
-          <img src="" alt="" />
+          <img src={this.props.image} alt={this.props.name + " " + this.props.lastName} />
         </div>
         <div className="text-container">
-          <h2 className="name"></h2>
-          <p className="city"></p>
-          <p className="age"></p>
+          <h2 className="name">{this.props.name + " " + this.props.lastName}</h2>
+          <p className="city">{this.props.city}</p>
+          <p className="age">{this.props.age}</p>
         </div>
       </>
     );
--- modulo-3/leccion-10/modulo-3-leccion-10-ejercicio-01-lucia/src/components/CardsList.js
+++ modulo-3/leccion-10/modulo-3-leccion-10-ejercicio-01-lucia/src/components/CardsList.js
@@ -4,7 +4,18 @@ import Cards from "./Cards";
 
 class CardsList extends React.Component {
   render() {
-    return <ul className="people-list"></ul>;
+    const peopleList = this.props.personInfo.map((item, index) => (
+      <li className="list" key={index}>
+        <Cards
+          image={item.picture.medium}
+          name={item.name.first}
+          lastName={item.name.last}
+          city={item.location.city}
+          age={item.dob.age}
+        />
+      </li>
+    ));
+    return <ul className="people-list">{peopleList}</ul>;
   }
 }
 CardsList.propTypes = {};
