51b910e447ffc9d7feb4d01082fff25eb65d362d
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-29 16:42:42 +0100

    Add 2 test more in a test suite

--- modulo-3/leccion-13/intro-testing/js/index.test.js
+++ modulo-3/leccion-13/intro-testing/js/index.test.js
@@ -1,5 +1,12 @@
 const sum = require("./index");
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("Sum App", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  test("adds -1 + 2 to equal 1", () => {
+    expect(sum(-1, 2)).toBe(1);
+  });
+  test("adds 0.22 + 0.78 to equal 1", () => {
+    expect(sum(0.22, 0.78)).toBe(1);
+  });
 });
