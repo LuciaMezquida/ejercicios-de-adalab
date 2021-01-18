e35ee0942caadab371c3d43ed8e0b69736ca7caa
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-18 15:55:54 +0100

    Write a file from node

--- modulo-4/introduccion-a-node/modulo-4-leccion-3-ejercicio-1/index.js
+++ modulo-4/introduccion-a-node/modulo-4-leccion-3-ejercicio-1/index.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const dataContent =
+  "Como quieres que te quiera si el que quiero que me quiera no me quiere como quiero que me quiera";
 
 fs.readFile("./input.txt", "utf-8", (err, fileContent) => {
   if (err !== null) {
@@ -7,6 +9,7 @@ fs.readFile("./input.txt", "utf-8", (err, fileContent) => {
   console.log("El contenido del fichero es: ", fileContent);
   console.log("La longitud del contenido es: ", fileContent.length);
 });
+
 fs.readFile("./input.json", "utf-8", (err, fileContent) => {
   if (err !== null) {
     console.log("Error: ", err);
@@ -14,4 +17,11 @@ fs.readFile("./input.json", "utf-8", (err, fileContent) => {
   const jsonData = JSON.parse(fileContent);
   console.log("El contenido del fichero es: ", jsonData.content);
   console.log("Es autor del contenido es: ", jsonData.author);
+});
+
+fs.writeFile("./output.txt", "utf-8", (err) => {
+  if (err !== null) {
+    console.log("Error: ", err);
+  }
+  console.log("The file has been saved");
 });
