fa42e74df887cbaeac81362dd23c88c882975c32
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-18 15:48:29 +0100

    Read json file

--- modulo-4/introduccion-a-node/modulo-4-leccion-3-ejercicio-1/index.js
+++ modulo-4/introduccion-a-node/modulo-4-leccion-3-ejercicio-1/index.js
@@ -7,3 +7,11 @@ fs.readFile("./input.txt", "utf-8", (err, fileContent) => {
   console.log("El contenido del fichero es: ", fileContent);
   console.log("La longitud del contenido es: ", fileContent.length);
 });
+fs.readFile("./input.json", "utf-8", (err, fileContent) => {
+  if (err !== null) {
+    console.log("Error: ", err);
+  }
+  const jsonData = JSON.parse(fileContent);
+  console.log("El contenido del fichero es: ", jsonData.content);
+  console.log("Es autor del contenido es: ", jsonData.author);
+});
