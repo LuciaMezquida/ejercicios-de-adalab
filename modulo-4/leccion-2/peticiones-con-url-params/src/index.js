7ad8bb45254aaeea1874127ce20bb9ca7920528b
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-19 19:25:10 +0100

    Install express, cors and create index.js

---
+++ modulo-4/leccion-2/peticiones-con-url-params/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+// create server
+const server = express();
+
+// set express middleware
+server.use(express.json());
+server.use(cors());
+
+// init express aplication
+const serverPort = 3000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// static server
+const staticServerPath = "./public";
+server.use(express.static(staticServerPath));
