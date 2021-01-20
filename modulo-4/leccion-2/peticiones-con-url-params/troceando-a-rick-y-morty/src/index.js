89ef00878b3b9df646c9494d30ec728e79f0e699
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-20 13:52:59 +0100

    Create an endpoint to get every info character

--- modulo-4/leccion-2/peticiones-con-url-params/troceando-a-rick-y-morty/src/index.js
+++ modulo-4/leccion-2/peticiones-con-url-params/troceando-a-rick-y-morty/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const users = require("./data.json");
+const data = require("./data.json");
+const users = data.results;
 
 // create server
 const server = express();
@@ -19,47 +20,36 @@ server.listen(serverPort, () => {
 const staticServerPath = "./public";
 server.use(express.static(staticServerPath));
 
-server.get("/users/all", (req, res) => {
-  res.json(users);
-});
-
-server.get("/users/:userId/orders/all", (req, res) => {
+server.get("/users/:userId", (req, res) => {
   console.log("Url params:", req.params);
   console.log("Url param userId:", req.params.userId);
-  console.log("Url param orders:", req.params.ordersId);
-
   // find promo by userId
-  const user = users.filter((user) => user.id === req.params.userId);
-  console.log("Found user:", user);
-  const order = user[0].order;
-  console.log("Found order:", order);
-
+  const user = users.find((user) => user.id === parseInt(req.params.userId));
+  console.log("user: " + user);
   // response with selected user data or error
   if (user === undefined) {
     res.json({ error: "user-not-found" });
-  } else {
-    res.json(order);
-  }
-});
-server.get("/users/:userId/orders/:ordersId", (req, res) => {
-  console.log("Url params:", req.params);
-  console.log("Url param userId:", req.params.userId);
-  console.log("Url param orders:", req.params.ordersId);
-
-  // find promo by userId
-  const user = users
-    .filter((user) => user.id === req.params.userId)
-    .find((order) => order.order[req.params.ordersId]);
-  console.log("Found user:", user);
-  const order = user.order[req.params.ordersId];
-  console.log("Found order:", order);
-
-  // response with selected user data or error
-  if (user === undefined) {
-    res.json({ error: "user-not-found" });
-  } else if (order !== undefined) {
-    res.json({ [req.params.ordersId]: order });
   } else {
     res.json(user);
   }
 });
+// server.get("/users/:userId/episodes", (req, res) => {
+//   console.log("Url params:", req.params);
+//   console.log("Url param userId:", req.params.userId);
+
+//   // find promo by userId
+//   const user = users
+//     .filter((user) => user.id === req.params.userId)
+//   const episodes = user.episode
+//   console.log("Found user:", user);
+//   console.log("Found episodes:", episodes);
+
+//   // response with selected user data or error
+//   if (user === undefined) {
+//     res.json({ error: "user-not-found" });
+//   } else if (episodes !== undefined) {
+//     res.json(episodes);
+//   } else {
+//     res.json(user);
+//   }
+// });
