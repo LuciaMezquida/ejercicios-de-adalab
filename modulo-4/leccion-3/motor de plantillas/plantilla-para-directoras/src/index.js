6c5ee895cb363f3a066bce8f08c4514c1bfaec2b
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-01-20 19:13:00 +0100

    Add template engine for directors

---
+++ modulo-4/leccion-3/motor de plantillas/plantilla-para-directoras/src/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const films = require("./data.json");
+const directors = require("./directors-data.json");
+
+// create app server
+const app = express();
+
+// set express middlewares
+app.use(express.json());
+app.use(cors());
+
+// set template engine middlewares
+app.set("view engine", "ejs");
+
+// init express aplication
+const serverPort = 3000;
+app.listen(serverPort, () => {
+  console.log(`App listening at http://localhost:${serverPort}`);
+});
+
+// endpoints
+
+app.get("/es/film:filmId.html", (req, res) => {
+  // get film data
+  const filmData = films.find((film) => film.id === req.params.filmId);
+  console.log("film data", filmData);
+  //ensure data
+  filmData.id = filmData.id || "";
+  filmData.title = filmData.title || "";
+  filmData.year = filmData.year || "";
+  filmData.director = filmData.director || "";
+  filmData.country = filmData.country || "";
+  filmData.awards = filmData.awards || [];
+
+  // response with rendered template called film
+  if (filmData) {
+    res.render("pages/film", filmData);
+  } else {
+    res.render("pages/film-not-found");
+  }
+});
+
+app.get("/es/directora/:directorId", (req, res) => {
+  const directorData = directors.find(
+    (director) => director.id === req.params.directorId
+  );
+  console.log("director data", directorData);
+  if (directorData) {
+    res.render("pages/director", directorData);
+  } else {
+    res.render("pages/film-not-found");
+  }
+});
